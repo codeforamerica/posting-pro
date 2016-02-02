96d0cc4b8725f7a161286939becac724b10cf2d6
Pascal Carole <pascal@rofocarole.local>
2016-02-02 11:22:26 -0800

    fix all the curly braces

--- asset/script/main.js
+++ asset/script/main.js
@@ -156,13 +156,13 @@
     function summaryOutputControl(element) {
         $(document).on('lint-results', function (event, results) {
             var tooHigh = results.readingLevel >= 9;
-            var readingLevelSummary =
-            {
+            var readingLevelSummary = {
               "readingLevel": results.readingLevel,
               "tooHigh": tooHigh,
               "level": tooHigh ? "warning" : "info"
             };
-            element.innerHTML = templates.readingLevel.render(readingLevelSummary});
+            element.innerHTML = templates.readingLevel.render(readingLevelSummary);
+        });
     };
 
     function generateLintId (results) {
