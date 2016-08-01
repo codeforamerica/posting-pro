8f7a81ebc3b4eb140010affb550c2f84350b6332
Pascal Carole <pacarole@gmail.com>
2016-08-01 11:24:27 -0700

    use env var for admin section

--- sinatra/sinatra_app.rb
+++ sinatra/sinatra_app.rb
@@ -47,7 +47,7 @@ helpers do
 
   def authorized?
     @auth ||=  Rack::Auth::Basic::Request.new(request.env)
-    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['admin', 'admin']
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == [ENV['ADMIN_USER'], ENV['ADMIN_PASSWORD']]
   end
 end
 
